bc2f87d1d7ef5a23c66396cddef438e9c400ddd3
Yash Bhandari <behumoury@gmail.com>
2022-08-20 16:04:01 +0530

    Fixed Bugs and added minmax functioin

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,6 +1,7 @@
-# import random
+
 import random
 
+#rnd function gives us random positions out of the empty positions(in human form).
 def rnd(l):
     list1 = []
     
@@ -15,18 +16,18 @@ def rnd(l):
     
     return a+1,b+1
 
+#Puts O in the position given (arguments in comp form)
 def puto(i,j,l):
-    for m in range(3):
-        for n in range(3):
-            if m==i and n==j:
-                l[m][n]=' O '
-                
+    l[i][j]=' O '
+
+#Puts space in the position given (arguments in comp form)
+def pute(i,j,l):
+    l[i][j]=' _ '
+#Puts X in the position given (arguments in comp form)            
 def putx(i,j,l):
-    for m in range(3):
-        for n in range(3):
-            if m==i and n==j:
-                l[m][n]=' X '
+    l[i][j]=' X '
                 
+# Prints the game board
 def pgame(l):
     for m in range(3):
         for n in range(3):
@@ -34,113 +35,6 @@ def pgame(l):
         print()
 
 #row+1 and col+1 because 0 == False
-
-def duo_check_hor(l,k):
-    #horizontal check
-    for m in range(3):
-        count=0
-        
-        for n in l[m]:    
-            if(n==k):
-                count+=1 
-                
-        if(count==2):
-            row=m
-            break
-        else:
-            count=0
-
-    if (count==2):
-        col=0
-        f=0
-        for m in l[row]:
-            
-            if(m==' _ '):
-                f=1
-                break
-            col+=1
-            
-        if f==1:        
-            return row+1,col+1
-    else: 
-        return False
-    
-def duo_check_ver(l,k):    
-    #vertical check
-    for m in range(3):
-        c=0
-        for n in range(3):
-            if l[n][m] == k:
-                c+=1
-        if c == 2:
-            col=m            
-            break
-        else:
-            c=0
-    row=0
-    if c == 2:
-        f=0
-        for m in range(3):
-            
-            if(l[m][col]==' _ '):
-                f=1
-                row=m
-                break
-            
-        if f==1:        
-            return row+1,col+1
-    else:
-        return False
-
-def duo_check_dig(l,k):    
-    #diagonal check
-    for m in range(3):
-        c=0
-        if l[m][m] == k:
-            c+=1
-        if c == 2:          
-            break
-        
-            
-
-    if c == 2:
-        for m in range(3):
-            f=0
-            
-            if(l[m][col]==' _ '):
-                f=1
-                row=m
-                col=m
-                break
-        if f==1:        
-            return row+1,col+1
-
-    #diagonal check 2
-
-    for m in range(3):
-        c=0
-        n=2-m
-        if l[m][n] == k:
-            c+=1
-        if c == 2:          
-            break
-        
-    
-    if c == 2:
-        f=0
-        for m in range(3):
-            
-            n=2-m
-            if(l[m][n]==' _ '):
-                f=1
-                row=m
-                col=n
-                break
-        if f==1:        
-            return row+1,col+1
-    else:
-        return False
-    
 def trio_check(l,k):
     #hori
     for m in range(3):
@@ -161,30 +55,30 @@ def trio_check(l,k):
             if l[n][m] == k:
                 c+=1
         if c == 3:
-            return True            
+            return True           
             
 
     #diagonal check 1
+    c=0
     for m in range(3):
-        c=0
         if l[m][m] == k:
             c+=1
         if c == 3:
-            return True          
-            break
+            return True
 
     #diagonal check 2
+    c=0
     for m in range(3):
-        c=0
+        
         n=2-m
         if l[m][n] == k:
             c+=1
         if c == 3:  
-            return True        
-            
-        else:
-            return False
+            return True            
+    
+    return False
 
+#prints a horizontal line to seprate the boards
 def hr():
     print()
     for x in range(10):
@@ -192,59 +86,98 @@ def hr():
     print()
     print()
 
-def duo_o(g):
-
-    if(duo_check_hor(g,' O ')):
-        a,b=duo_check_hor(g,' O ')
-        return a,b
-
-    elif(duo_check_ver(g,' O ')):
-        a,b=duo_check_ver(g,' O ')
-        return a,b
-
-    elif(duo_check_dig(g,' O ')):
-        a,b=duo_check_dig(g,' O ')
-        return a,b
-
-    else: 
-        return False
-
-def duo_x(g):
-
-    if(duo_check_hor(g,' X ')):
-        a,b=duo_check_hor(g,' X ')
-        return a,b
-
-    elif(duo_check_ver(g,' X ')):
-        a,b=duo_check_ver(g,' X ')
-        return a,b
-
-    elif(duo_check_dig(g,' X ')):
-        a,b=duo_check_dig(g,' X ')
-        return a,b
-
-    else: 
-        return False
-
-def victory(g):
-    if(trio_check(game,' O ')):
-        hr()
-        print('You Won')
-        return False
-    elif(trio_check(game,' X ')):
-        hr()
-        print('You Lost')
-        return False
-    else:
-        return True
-
+#Check if the given index is empty
 def ifempty(m,n,l):
     if l[m][n]==' _ ':
         return True
     else:
         return False
 
+def UserInput(game):
+    i=int(input('enter row[1,2,3] = '))
+    j=int(input('enter col[1,2,3] = '))
+    if(ifempty(i-1,j-1,game)):
+        puto(i-1,j-1,game)
+        return 0
+    else:
+        print('invalid position!!')
+        i=int(input('enter row[0,1,2] = '))
+        j=int(input('enter col[0,1,2] = '))
+        if(ifempty(i-1,j-1,game)):
+            puto(i-1,j-1,game)
+            return 0
+        else:
+            print('Tu mat hi khel bye!!!!')
+            return 1
+ 
+def emsp(l):
+    list1 = []#list of all the empty positions
+    
+    for m in range(3):
+        for n in range(3):
+            if l[m][n]==' _ ':
+                list1.append([m,n])
 
+    return list1
+
+
+def score(g):
+    if(trio_check(game,' O ')):
+        return -1
+    elif(trio_check(game,' X ')):
+        return 1
+    else:
+        return 0
+
+
+
+
+def minmax(game,depth,maxiplay): # -1 0 1
+    if score(game)!=0:
+        return score(game)
+
+    if maxiplay:
+        maxEval = -5
+        
+        child = emsp(game)
+        for ch in child:
+            putx(ch[0],ch[1],game)
+            e = minmax(game,depth-1,True)
+            maxEval = max(maxEval,e)
+            pute(ch[0],ch[1],game)
+        return maxEval
+
+    else:
+        minEval = 5
+        child = emsp(game)
+        for ch in child:
+            puto(ch[0],ch[1],game)
+            e = minmax(game,depth-1,False)
+            minEval = min(minEval,e)
+            pute(ch[0],ch[1],game)
+        return minEval
+
+
+
+
+def bestMove(game):
+    list1 = emsp(game) #list of all the empty positions
+    bestScore = -5
+
+    for posi in list1:
+        game[posi[0]][posi[1]]=' X '
+        evalu = minmax(game,3,False)
+        game[posi[0]][posi[1]]=' _ '
+        if evalu > bestScore:
+            bestScore = evalu
+            r = posi
+
+    return r[0],r[1]
+
+
+
+                
+#game ahead
 
 game= [[' _ ',' _ ',' _ '],
        [' _ ',' _ ',' _ '],
@@ -252,55 +185,53 @@ game= [[' _ ',' _ ',' _ '],
 pgame(game)
     
 t=9
-while(victory(game) and t):
-    
-    hr()
-    #user input
-    i=int(input('enter row[1,2,3] = '))
-    j=int(input('enter col[1,2,3] = '))
-    if(ifempty(i-1,j-1,game)):
-        puto(i-1,j-1,game)
-    else:
-        print('invalid position!!')
-        i=int(input('enter row[0,1,2] = '))
-        j=int(input('enter col[0,1,2] = '))
-        if(ifempty(i-1,j-1,game)):
-            puto(i-1,j-1,game)
-        else:
-            print('Tu mat hi khel bye!!!!')
-            break
-
-    pgame(game)
-    t-=1
+while(True):
+    if score(game) == 1:
+        hr()
+        print ('You lost')
+        break
+    if score(game) == -1:
+        hr()
+        print ('You Won')
+        break
     if(t==0):
         hr()
-        print("It's a Draw")
+        print("It's a Draw here")
         break
-    
+    hr()
+
+    #user input
+    if UserInput(game):
+        print('hao bhaiya bye!!')
+        break
+
+    pgame(game)
+    t=-1
+
+    if score(game) == 1:
+        hr()
+        print ('You lost')
+        break
+    if score(game) == -1:
+        hr()
+        print ('You Won')
+        break
+    if(t==0):
+        hr()
+        print("It's a Draw here")
+        break
+    hr()
+
+
     #our input
-    
-
-    if(duo_x(game)):
-        a,b=duo_x(game)
-        if(ifempty(a-1,b-1,game)):
-            putx(a-1,b-1,game)
-        else:
-            a,b=rnd(game)
-            putx(a-1,b-1,game)
-            
-    elif(duo_o(game)):
-        a,b=duo_o(game)
-        print(a,b)
-        if(ifempty(a-1,b-1,game)):
-            putx(a-1,b-1,game)
-        else:
-            a,b=rnd(game)
-            putx(a-1,b-1,game)
-
-    else:
-        a,b=rnd(game)
-        putx(a-1,b-1,game)
+    a,b=bestMove(game)
+    #a,b=rnd(game)
+    #a-=1
+    #b-=1
+    putx(a,b,game)
 
     hr()
     pgame(game)
-    t-=1
+    t=-1
+
+
